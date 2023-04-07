2c72a23e845806975ed5d489301e26bb07d1174a
George Smith <georgepetersmith@outlook.com>
2023-04-07 20:33:58 +0100

    Add day one calorie counting in rust

---
+++ rust/calorie_counting/src/main.rs
@@ -0,0 +1,31 @@
+use std::env;
+use std::fs::File;
+use std::io::{prelude::*, BufReader, Result};
+
+fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
+    let file = File::open(file_path)?;
+    let reader = BufReader::new(file);
+
+    let mut calorie_count = 0;
+    let mut highest_calorie_count = 0;
+
+    for line in reader.lines() {
+        if let Ok(calories) = line {
+            if calories.is_empty() {
+                if calorie_count > highest_calorie_count {
+                    highest_calorie_count = calorie_count;
+                }
+
+                calorie_count = 0;
+
+                continue;
+            }
+
+            calorie_count += calories.parse::<i32>().unwrap();
+        }
+    }
+
+    Ok(())
+}
