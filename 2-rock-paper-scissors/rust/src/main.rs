baf7a4f49182e96adf6868a08d6fce9b4afb31b1
George Smith <georgepetersmith@outlook.com>
2023-04-09 17:21:28 +0100

    Replace char hashmap wth structs and enums to represent moves and outcomes

--- 2-rock-paper-scissors/rust/src/main.rs
+++ 2-rock-paper-scissors/rust/src/main.rs
@@ -1,12 +1,45 @@
-use std::collections::HashMap;
 use std::env;
 use std::fs::File;
 use std::io::{prelude::*, BufReader, Result};
+use std::str::FromStr;
+
+#[derive(PartialEq)]
+struct Move {
+    shape: Shape,
+    score: i32,
+    beats: Shape
+}
+
+#[derive(PartialEq)]
+enum Shape {
+    Rock,
+    Paper,
+    Scissors
+}
+
+enum Outcome {
+    Win,
+    Lose,
+    Draw
+}
+
+#[derive(Debug)]
+struct ParseMoveError;
+
+impl FromStr for Move {
+    type Err = ParseMoveError;
+
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        match s {
+            "A" | "X" => Ok(Move {shape: Shape::Rock, score: 1, beats: Shape::Scissors}),
+            "B" | "Y" => Ok(Move {shape: Shape::Paper, score: 2, beats: Shape::Rock}),
+            "C" | "Z" => Ok(Move {shape: Shape::Scissors, score: 3, beats: Shape::Paper}),
+            _ => Err(ParseMoveError)
+        }
+    }
+}
 
 fn main() -> Result<()> {
-    let shape_scores: HashMap<char, i32> = HashMap::from([('A', 1), ('X', 1), ('B', 2), ('Y', 2), ('C', 3), ('Z', 3)]);
-    let outcome_scores: HashMap<&str, i32> = HashMap::from([("LOST", 0), ("DREW", 3), ("WON", 6)]);
-
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
     let file = File::open(file_path)?;
@@ -14,42 +47,27 @@ fn main() -> Result<()> {
 
     let mut accum_score = 0;
     for line in reader.lines() {
-        if let Ok(strategy) = line {
-            let my_shape = strategy.chars().nth(2).unwrap();
-            let shape_score = shape_scores.get(&my_shape).unwrap();
+        if let Ok(round) = line {
+            let moves: Vec<Move> = round.split(' ').map(|s| s.parse().unwrap()).collect();
+            let my_move = moves.get(1).expect("Failed to get my move");
+            let opponent_move = moves.get(0).expect("Failed to get opponent's move");
 
-            let opponent_shape = strategy.chars().nth(0).unwrap();
-            let outcome = match my_shape {
-                'X' => {
-                    match opponent_shape {
-                        'A' => "DREW",
-                        'B' => "LOST",
-                        'C' => "WON",
-                        _ => "ERROR"
-                    }
-                },
-                'Y' => {
-                    match opponent_shape {
-                        'A' => "WON",
-                        'B' => "DREW",
-                        'C' => "LOST",
-                        _ => "ERROR"
-                    }
-                },
-                'Z' => {
-                    match opponent_shape {
-                        'A' => "LOST",
-                        'B' => "WON",
-                        'C' => "DREW",
-                        _ => "ERROR"
-                    }
-                },
-                _ => "ERROR"
+            let outcome;
+            if my_move.beats == opponent_move.shape {
+                outcome = Outcome::Win;
+            } else if my_move.shape == opponent_move.shape {
+                outcome = Outcome::Draw;
+            } else {
+                outcome = Outcome::Lose;
+            }
+
+            let outcome_score = match outcome {
+                Outcome::Win => 6,
+                Outcome::Draw => 3,
+                Outcome::Lose => 0
             };
 
-            let outcome_score = outcome_scores.get(&outcome).unwrap();
-
-            let score = shape_score + outcome_score;
+            let score = my_move.score + outcome_score;
             accum_score += score;
         }
     }
