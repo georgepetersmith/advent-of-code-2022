fb9d203aede66c3541d003d98a2ab4cca3935927
George Smith <georgepetersmith@outlook.com>
2023-04-11 20:45:50 +0100

    Add solution to day 3 part 2

--- 3-rucksack-reorganisation/rust/src/main.rs
+++ 3-rucksack-reorganisation/rust/src/main.rs
@@ -5,7 +5,13 @@ use std::fs::File;
 use std::io::{prelude::*, BufReader};
 use substring::Substring;
 
+
 fn main() {
+    let file_path = env::args().nth(1).unwrap();
+    let file = File::open(file_path).unwrap();
+    let reader = BufReader::new(file);
+    let lines = reader.lines().map(|s| s.unwrap()).collect::<Vec<String>>();
+
     let mut priorities = HashMap::new();
     {
         let mut priority = 1;
@@ -19,13 +25,13 @@ fn main() {
         }
     }
 
-    let file_path = env::args().nth(1).unwrap();
-    let file = File::open(file_path).unwrap();
-    let reader = BufReader::new(file);
+    part_one(&lines, &priorities);
+    part_two(&lines, &priorities);
+}
 
+fn part_one(lines: &Vec<String>, priorities: &HashMap<char, i32>) {
     let mut accum = 0;
-    for line in reader.lines() {
-        let contents = line.unwrap();
+    for contents in lines.iter() {
         let rucksack_size = contents.len();
         let compartment_size = rucksack_size / 2;
 
@@ -46,5 +52,36 @@ fn main() {
         accum += shared_priority;
     }
 
-    println!("{}", accum);
+    println!("Part One: {}", accum);
+}
+
+fn part_two(lines: &Vec<String>, priorities: &HashMap<char, i32>) {
+    let mut groups: Vec<[&str; 3]> = Vec::new();
+    let mut group: [&str; 3] = ["", "", ""];
+    let mut slice_iter = 0;
+    for contents in lines.iter() {
+        group[slice_iter] = contents.as_str();
+        slice_iter += 1;
+        if slice_iter == 3 {
+            groups.push(group.clone());
+            group = ["", "", ""];
+            slice_iter = 0;
+        }
+    }
+
+    let mut accum = 0;
+    let mut group_badge = None;
+    for g in groups {
+        for c in g[0].chars() {
+            if g[1].contains(c) && g[2].contains(c) {
+                group_badge = Some(c);
+                break;
+            }
+        }
+
+        let priority = priorities[&group_badge.unwrap()];
+        accum += priority;
+    }
+
+    println!("Part Two: {}", accum);
 }
