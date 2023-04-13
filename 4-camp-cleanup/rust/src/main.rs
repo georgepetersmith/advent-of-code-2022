ae2c027fb7601eb040785b7e7838782dc992aab5
George Smith <georgepetersmith@outlook.com>
2023-04-13 21:13:12 +0100

    Add day 4 part 2 solution

--- 4-camp-cleanup/rust/src/main.rs
+++ 4-camp-cleanup/rust/src/main.rs
@@ -7,7 +7,8 @@ fn main() {
     let file = File::open(file_path).unwrap();
     let reader = BufReader::new(file);
 
-    let mut count = 0;
+    let mut part_one_count = 0;
+    let mut part_two_count = 0;
     for line_result in reader.lines() {
         let line = line_result.unwrap();
         let sections = line.split(',').collect::<Vec<&str>>();
@@ -38,9 +39,19 @@ fn main() {
             || (last_section_lower <= first_section_lower
                 && last_section_upper >= first_section_upper)
         {
-            count += 1;
+            part_one_count += 1;
+            part_two_count += 1;
+        } else if first_section_lower <= last_section_lower
+            && last_section_lower <= first_section_upper
+        {
+            part_two_count += 1;
+        } else if last_section_lower <= first_section_lower
+            && first_section_lower <= last_section_upper
+        {
+            part_two_count += 1;
         }
     }
 
-    println!("{}", count);
+    println!("Part One: {}", part_one_count);
+    println!("Part One: {}", part_two_count);
 }
