fb94d840ebe878fe7893003838966528ac54faee
George Smith <georgepetersmith@outlook.com>
2023-04-14 22:16:12 +0100

    Add solution to day 1 part 2

--- 1-calorie-counting/rust/src/main.rs
+++ 1-calorie-counting/rust/src/main.rs
@@ -1,26 +1,30 @@
 use std::env;
 use std::fs::File;
 use std::io::{prelude::*, Result};
+use itertools::Itertools;
 
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
-    let mut file = File::open(file_path)?;
-
+    let mut file = File::open(&file_path)?;
     let mut buffer = String::new();
     file.read_to_string(&mut buffer)?;
 
-    let calorie_count = buffer
+    let calories = buffer
         .split("\n\n")
         .map(|elf| {
             elf.lines()
                 .map(|calorie| calorie.parse::<u32>().unwrap())
                 .sum::<u32>()
         })
-        .max()
-        .unwrap();
+        .sorted_by(|a, b| b.cmp(a))
+        .collect::<Vec<_>>();
 
-    println!("{}", calorie_count);
+    let max_calories = calories.iter().max().expect("Error");
+    let top_three_calories_sum = calories.iter().take(3).sum::<u32>();
+
+    println!("Part One: {}", max_calories);
+    println!("Part Two: {}", top_three_calories_sum);
 
     Ok(())
 }
