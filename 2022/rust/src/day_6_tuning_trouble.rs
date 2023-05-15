5f052dabb65dc75c0deef5946ff8cb1d5424c091
George Smith <georgepetersmith@outlook.com>
2023-05-15 21:32:52 +0100

    Add day 6 part 2 solution

--- 2022/rust/src/day_6_tuning_trouble.rs
+++ 2022/rust/src/day_6_tuning_trouble.rs
@@ -2,25 +2,32 @@ use std::collections::HashSet;
 
 pub fn run_part(part: u8, input: &str) -> String {
     match part {
-        1 => part_one(input),
+        1 => part_one(input).to_string(),
+        2 => part_two(input).to_string(),
         _ => panic!("There is no part {} for this day", part),
     }
 }
 
-fn part_one(input: &str) -> String {
+fn part_one(input: &str) -> usize {
+    search_marker_index(input, 4)
+}
+
+fn part_two(input: &str) -> usize {
+    search_marker_index(input, 14)
+}
+
+fn search_marker_index(input: &str, len: usize) -> usize {
     let mut start_index = 0;
-    let mut end_index = 4;
-    let mut ans = 0;
+    let mut end_index = len;
 
     while end_index < input.len() {
         let slice = input
             .chars()
             .skip(start_index)
-            .take(4)
+            .take(len)
             .collect::<HashSet<char>>();
 
-        if slice.len() == 4 {
-            ans = end_index;
+        if slice.len() == len {
             break;
         }
 
@@ -28,7 +35,7 @@ fn part_one(input: &str) -> String {
         end_index = end_index + 1;
     }
 
-    ans.to_string()
+    end_index
 }
 
 #[cfg(test)]
@@ -37,9 +44,17 @@ mod tests {
 
     #[test]
     fn part_one_test() {
-        let expect = "5".to_owned();
-        let sut = "bvwbjplbgvbhsrlpgdmjqwftvncz";
-        let result = part_one(sut);
+        let expect = 5;
+        let input = "bvwbjplbgvbhsrlpgdmjqwftvncz";
+        let result = part_one(input);
+        assert_eq!(result, expect);
+    }
+
+    #[test]
+    fn part_two_test() {
+        let expect = 19;
+        let input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+        let result = part_two(input);
         assert_eq!(result, expect);
     }
 }
